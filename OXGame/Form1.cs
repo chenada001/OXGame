3948bb91e3d0f052158730bd2ed2ffed94843b0d
妤瑄 陳 <polk931020@gmail.com>
2024-03-26 21:11:11 +0800

    加入專案檔案。

---
+++ OXGame/Form1.cs
@@ -0,0 +1,25 @@
+namespace OXGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void s1120367(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void player_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
